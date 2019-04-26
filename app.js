da069ecb46dcc2b116f891a16e5662ff8c69a568
Rebecca Uranga <urangarebecca@gmail.com>
2019-04-25 20:13:53 -0400

    adds data object to track expenses and incomes

--- app.js
+++ app.js
@@ -1,40 +1,116 @@
-// BUDGET CONTROLLER 
+///////// BUDGET CONTROLLER /////////
 
 const budgetController = (function () {
 
-    // Some code
+        var Expense = function(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+    
+        var Income = function(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+    
+        var data = {
+            allItems: {
+                exp: [],
+                inc: []
+            },
+            totals: {
+                exp: 0,
+                inc: 0
+            }
+        }
+    
+        return {
+            addItem: function(type, des, val) {
+                var newItem;
+    
+                ID = 0;
+    
+                if (type === 'exp') {
+                    newItem = new Expense(ID, des, val);
+                } else if (type === 'inc') {
+                    newItem = new Income(ID, des, val);
+                }
+    
+    
+                data.allItems[type].push(newItem);
+                return newItem;
+    
+            }
+        };
+    
+    
+    })();
+    
 
-})();
-
-// UI CONTROLLER
+///////// UI CONTROLLER /////////
 
 const UIController = (function() {
 
-    // Some code 
+    const DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, // Will be either `inc` or `exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+        getDOMStrings: function() {
+            return DOMStrings;
+        }
+    } 
 
 })();
 
-// GLOBAL APP CONTROLLER 
+///////// GLOBAL APP CONTROLLER /////////
 
 const controller = (function(budgetCtrl, UICtrl) {
 
+    const setUpEventListeners = function () {
+        const DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+
     const ctrlAddItem = function() {
         // 1. Get the field input data
+        const input = UICtrl.getInput();
+
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI 
         // 4. Calculate the budget
         // 5. Display the budget
-    }
+    };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('Application has started');
+            setUpEventListeners();
         }
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
 
 
 
