bdc072bc8bee275807bbc8f6f4a4337b8dd9a97e
Rebecca Uranga <urangarebecca@gmail.com>
2019-04-25 21:47:04 -0400

    renders input onto the DOm

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+
 ///////// BUDGET CONTROLLER /////////
 
 const budgetController = (function () {
@@ -27,20 +30,32 @@ const budgetController = (function () {
     
         return {
             addItem: function(type, des, val) {
-                var newItem;
-    
-                ID = 0;
-    
+                var newItem, ID;
+                
+                // Create new ID
+                if (data.allItems[type].length > 0) {
+                    ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+                } else {
+                    ID = 0;
+                }
+                
+                // Create new item based on 'inc' or 'exp' type
                 if (type === 'exp') {
                     newItem = new Expense(ID, des, val);
                 } else if (type === 'inc') {
                     newItem = new Income(ID, des, val);
                 }
     
-    
+                // Push it into our data structure
                 data.allItems[type].push(newItem);
+
+                // Return the new element
                 return newItem;
     
+            },
+
+            testing: function() {
+                console.log(data);
             }
         };
     
@@ -56,7 +71,9 @@ const UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return {
@@ -67,6 +84,29 @@ const UIController = (function() {
                 value: document.querySelector(DOMStrings.inputValue).value
             };
         },
+
+        addListItem: function(obj, type) {
+            var html, newHTML, element;
+            
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
+                element = DOMStrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            // Replace the placeholder text with some actual data
+            newHTML = html.replace('%id%', obj.id);
+            newHTML = newHTML.replace('%description%', obj.description);
+            newHTML = newHTML.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
+
+        },
+
         getDOMStrings: function() {
             return DOMStrings;
         }
@@ -92,12 +132,19 @@ const controller = (function(budgetCtrl, UICtrl) {
 
 
     const ctrlAddItem = function() {
+        var input, newItem;
+
         // 1. Get the field input data
-        const input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        var newItem = budgetController.addItem(input.type, input.description, input.value);
+
         // 3. Add the item to the UI 
+        UICtrl.addListItem(newItem, input.type);
+
         // 4. Calculate the budget
+
         // 5. Display the budget
     };
 
