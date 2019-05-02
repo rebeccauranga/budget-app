85a50af4397857e1113d5e3e77ebe8e798fc71d5
Rebecca Uranga <urangarebecca@gmail.com>
2019-05-01 22:10:16 -0400

    renders percentage on expense html

--- app.js
+++ app.js
@@ -163,7 +163,8 @@ const UIController = (function() {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     };
 
     return {
@@ -230,8 +231,23 @@ const UIController = (function() {
 
         },
 
-        displayPercentages: function() {
-            
+        displayPercentages: function(percentages) {
+
+            var fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback) {
+               for (var i = 0; i < list.length; i++) {
+                   callback(list[i], i);
+               }
+            }
+
+            nodeListForEach(fields, function(current, index){
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            })
         },
 
         getDOMStrings: function() {
@@ -279,7 +295,7 @@ const controller = (function(budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages 
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     }
 
 
